3edc1b5ba8711cbbc7d275fc0da64c4432061167
Turtleion <97813862+turtleion@users.noreply.github.com>
2022-09-03 18:21:49 +0700

    DEV 1 FINAL

--- MC/LINUX/run.sh
+++ MC/LINUX/run.sh
@@ -2,6 +2,6 @@
 if [ -x /usr/bin/java ] || [ -x /sbin/java ] || [ -x /bin/bash ]; then
   java -jar server.jar nogui
 else
-  echo "Please install java runtime and jdk first before run this script"
+  echo "Please install java runtime and jdk first before run this script (RECOMMENDED JDK VERSION IS 17)"
 fi
 
