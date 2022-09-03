697bc38f6ee29eb6754c06690a98d57ec937ff82
Turtleion <97813862+turtleion@users.noreply.github.com>
2022-09-03 16:36:28 +0700

    DEV 1

--- MC/LINUX/downloader.sh
+++ MC/LINUX/downloader.sh
@@ -1,4 +1,5 @@
 curl "https://api.purpurmc.org/v2/purpur/1.18.2/latest/download" -o server.jar 
-echo run on run.sh using ./run.sh
+echo run the script using ./run.sh
+echo "if error permission, you can add execute permission to the script using : chmod +x run.sh"
 pause
 exit
