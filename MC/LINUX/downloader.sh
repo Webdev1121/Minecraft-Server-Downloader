400b5d052e5b9fbd1b925e352538af8e092804cc
Turtleion <97813862+turtleion@users.noreply.github.com>
2022-09-03 17:29:35 +0700

    DEV 1 REV 1

--- MC/LINUX/downloader.sh
+++ MC/LINUX/downloader.sh
@@ -1,5 +1,12 @@
+#!/bin/bash
 curl "https://api.purpurmc.org/v2/purpur/1.18.2/latest/download" -o server.jar 
 echo run the script using ./run.sh
 echo "if error permission, you can add execute permission to the script using : chmod +x run.sh"
-pause
-exit
+if [ -f server.jar ]; then
+  mkdir server
+  mv server.jar server/
+  mv run.sh server/
+  cd server
+else
+  echo "Failed to setup (SCRIPT ERROR)"
+fi
